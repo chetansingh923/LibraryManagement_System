18486932322b4decfa0a876a5d18450439a6b3a8
Chetan Singh <149692629+chetansingh923@users.noreply.github.com>
2025-06-25 19:21:26 +0530

    Update Connect.java

--- src/Connect.java
+++ src/Connect.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author Sudhir Kushwaha
- */
+
 public class Connect {
     static Connection con=null;
     public static Connection ConnectToDB() {
