0b1dd3dd4d1fd4857e61ac7eaba5dc44bce13a83
Chetan Singh <149692629+chetansingh923@users.noreply.github.com>
2025-06-25 19:23:45 +0530

    Update IssueBook.java

--- src/IssueBook.java
+++ src/IssueBook.java
@@ -9,16 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-
-/**
- *
- * @author Sudhir Kushwaha
- */
 public class IssueBook extends javax.swing.JFrame {
         PreparedStatement pst;
         ResultSet rs;
